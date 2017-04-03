11b48893a56f57278f30ce3f75c19e9133e738ce
yoff <rasmus@qvitoo.com>
2017-04-03 15:30:43 +0200

    sketch of level data

---
+++ spider-p2/js/level1.js
@@ -0,0 +1,15 @@
+function create(game) {
+    game.level1 = {
+        width: 30,
+        height: 80,
+        staticPlatforms: [
+            { x: 10, y: 20, width: 50, height: 20, deadly: false }
+        ],
+        movingPlatforms: [
+
+        ],
+        enemies: [
+
+        ]
+    }
+}
