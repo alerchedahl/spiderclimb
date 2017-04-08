7f639a8d4b5be0e3e8625766542ef4434e05bb05
yoff <rasmus@qvitoo.com>
2017-04-08 21:43:55 +0100

    start from level 1

--- spider-p2/js/startgame.js
+++ spider-p2/js/startgame.js
@@ -4,7 +4,7 @@ StartGame.prototype = {
 
 	create: function(){
 		this.game.state.lives = 5;
-		this.game.state.level = 5;
+		this.game.state.level = 1;
 		this.game.state.maxLevel = levels.length-1;
 		this.game.state.start("Main");
 	}
