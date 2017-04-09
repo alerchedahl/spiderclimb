71ad0e18cc15e45fbc4ff51977fbbb48501ac28d
yoff <rasmus@qvitoo.com>
2017-04-09 01:04:08 +0100

    less blurry text

--- spider-p2/js/globals.js
+++ spider-p2/js/globals.js
@@ -4,7 +4,7 @@ function titleText(game, title) {
     var style = { font: "bold 32px Arial", fill: "#567", boundsAlignH: "center", boundsAlignV: "middle" };
 
     //  The Text is positioned at 0, 150
-    text = game.add.text(0, 0, title, style);
+    text = game.add.text(0.5, 0.5, title, style);
     // text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
 
     //  We'll set the bounds to be from x0, y100 and be 800px wide by 100px high
@@ -15,7 +15,7 @@ function subtitleText(game, subtitle) {
     var style = { font: "bold 16px Arial", fill: "#395", boundsAlignH: "center", boundsAlignV: "middle" };
 
     //  The Text is positioned at 0, 300
-    text = game.add.text(0, 0, subtitle, style);
+    text = game.add.text(0.5, 0.5, subtitle, style);
     // text.setShadow(1, 1, 'rgba(0,0,0,0.5)', 2);
 
     //  We'll set the bounds to be from x0, y100 and be 800px wide by 100px high
