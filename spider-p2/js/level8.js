358389391d2510ab7f5f8818705a5aa7308dc504
Rasmus Lerchedahl Petersen <lerchedahl@gmail.com>
2023-03-02 21:01:03 +0100

    add level

--- spider-p2/js/level8.js
+++ spider-p2/js/level8.js
@@ -1,12 +1,15 @@
-levels[1] = {
+levels[8] = {
     width: 800,
     height: 600,
     start: { x: 50, y: 370 },
     staticPlatforms: [
-        { x: 400, y: 200, width: 200, height: 32, block: 'default' },
+        { x: 400, y: 300, width: 400, height: 32, block: 'default' },
         { x: 100, y: 400, width: 200, height: 32, block: 'default' },
         { x: 700, y: 400, width: 200, height: 32, block: 'default' },
-        { x: 400, y: 584, width: 800, height: 32, block: 'default' },
+        { x: 400, y: 482, width: 400, height: 32, block: 'fire' },
+        { x: 184, y: 457, width: 32, height: 82, block: 'fire' },
+        { x: 616, y: 457, width: 32, height: 82, block: 'fire' },
+        { x: 216, y: 268, width: 32, height: 32, block: 'fire' },
         // borders
         { x: -50, y: 100, width: 100, height: 1000, block: 'nonstick'},
         { x: 850, y: 100, width: 100, height: 1000, block: 'nonstick'},
@@ -19,9 +22,6 @@ levels[1] = {
         { x: 776, y: 368 }
     ],
     enemies: [
-
-    ],
-    hints: [
 
     ]
 }
